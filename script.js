1a692c671e93bbe5b90f88abb158031abe0b2410
sujuuu143 <sujalchoudhary592@gmail.com>
2024-09-03 13:33:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+var dark = document.getElementById("clicked");
+var white = document.getElementById("white-color");
+var lol = document.getElementById("white-2color");
+dark.onclick = function() {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        white.style.filter = "brightness(5)";
+        lol.style.filter = "brightness(5)";
+    } else {
+        white.style.filter = "none";
+        lol.style.filter = "none";
+    }
 }
+
+
+profile.addentListener('cilck', () => {
+            homePage.
+            profile.onclick = function() {
+
+
+            }
